85105fa64cf7b1cf1868a78b0101617753d4f585
abeycity4u <abeycity4u2002@yahoo.com>
2026-09-03 23:12:39 +0100

    Load inventory permission helpers in stock history API

--- api/get_stock_history.php
+++ api/get_stock_history.php
@@ -1,6 +1,7 @@
 <?php require_once(dirname(__DIR__) . '/init.php'); ?>
 <?php
 require_once(__DIR__ . '/../config.php');
+require_once(__DIR__ . '/../includes/functions.php');
 require_once(__DIR__ . '/../lib/stock_service.php');
 requireLogin();
 header('Content-Type: application/json');
