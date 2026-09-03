11e563b6f79913c17b78a50202225249129425ad
abeycity4u <abeycity4u2002@yahoo.com>
2026-09-03 23:35:31 +0100

    Enforce ruminant scope on record check API

--- api/check_ruminant_record.php
+++ api/check_ruminant_record.php
@@ -5,6 +5,13 @@ requireLogin();
 
 header('Content-Type: application/json');
 
+$isOwnerOrAdmin = isPlatformOwner() || hasRole('farm_admin');
+if (!$isOwnerOrAdmin && !checkAccess('ruminant')) {
+    http_response_code(403);
+    echo json_encode(['error' => 'You do not have access to the ruminant module.']);
+    exit;
+}
+
 if (isset($_GET['date']) && isset($_GET['animal_type'])) {
     $date = $_GET['date'];
     $animalType = strtolower(trim($_GET['animal_type']));
